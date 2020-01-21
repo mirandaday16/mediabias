f09a741225c4ec34953cb825ac3bd5b1fef90f61
mirandadayadkins <mirandaday16@gmail.com>
2020-01-21 15:39:44 -0500

    finds missing headlines, identifies videos from NYT

--- Article_Crawler.py
+++ Article_Crawler.py
@@ -22,26 +22,30 @@ def get_url(json_file_path: str):
     print("URL: " + url)
     return url
 
+
 def get_headline(json_file_path: str):
     json_obj = json.load(open(json_file_path))
     headline = json_obj["title"]
-    return headline
-
-# Determines whether media is an image or video
-def get_media_type(url):
-    req = Request(url, headers={'User-Agent': 'Mozilla/5.0'})
-    try:
-        page_content = urlopen(req).read()
-    except urllib.error.HTTPError:
-        return "HTTP Error"
-    except urllib.error.URLError:
-        return "URL Error"
+    if headline != "":
+        return headline
+    # The below accounts for rare instances where article headline was not recorded in json file
     else:
-        soup = BeautifulSoup(page_content, "html.parser")
-        if soup.find('div',{'class': 'video-container'}) is not None:
-            return 'VIDEO'
-        elif soup.find('div',{'class': 'm'}) is not None:
-            return 'IMAGE'
+        req = Request(get_url(json_file_path), headers={'User-Agent': 'Mozilla/5.0'})
+        try:
+            page_content = urlopen(req).read()
+        except urllib.error.HTTPError:
+            return "HTTP Error"
+        except urllib.error.URLError:
+            return "URL Error"
+        else:
+            soup = BeautifulSoup(page_content, "html.parser")
+            header = soup.find('h1')
+            if header is not None:
+                headline = header.find("span")
+                if headline is not None:
+                    headline_text = str(headline.getText())
+                return headline_text
+
 
 # Returns media link (image or video)
 def get_media(url):
@@ -59,6 +63,24 @@ def get_media(url):
         elif soup.find('img') is not None:
             return soup.find('img')['src']
 
+
+# FOX: Determines whether media is an image or video
+def fox_get_media_type(url):
+    req = Request(url, headers={'User-Agent': 'Mozilla/5.0'})
+    try:
+        page_content = urlopen(req).read()
+    except urllib.error.HTTPError:
+        return "HTTP Error"
+    except urllib.error.URLError:
+        return "URL Error"
+    else:
+        soup = BeautifulSoup(page_content, "html.parser")
+        if soup.find('div',{'class': 'video-container'}) is not None:
+            return 'VIDEO'
+        elif soup.find('div',{'class': 'm'}) is not None:
+            return 'IMAGE'
+
+
 # FOX: Gets alt text of first image/video in the article, if it exists
 def fox_get_alt_text(url):
     req = Request(url, headers={'User-Agent': 'Mozilla/5.0'})
@@ -70,13 +92,13 @@ def fox_get_alt_text(url):
         return "URL Error"
     else:
         soup = BeautifulSoup(page_content, "html.parser")
-        if get_media_type(url) == "IMAGE":
+        if fox_get_media_type(url) == "IMAGE":
             for div in soup.find_all('div'):
                 img = div.find('img', alt=True)
                 if img is not None:
                     if img['alt'] is not None:
                         return img['alt']
-        elif get_media_type(url) == "VIDEO":
+        elif fox_get_media_type(url) == "VIDEO":
             for div in soup.find_all('div'):
                 vid = div.find('video', alt=True)
                 if vid is not None:
@@ -107,6 +129,24 @@ def fox_get_captions_text(url):
                         return cap_text
 
 
+# NYT: Determines whether media is an image or video
+def nyt_get_media_type(url):
+    req = Request(url, headers={'User-Agent': 'Mozilla/5.0'})
+    try:
+        page_content = urlopen(req).read()
+    except urllib.error.HTTPError:
+        return "HTTP Error"
+    except urllib.error.URLError:
+        return "URL Error"
+    else:
+        soup = BeautifulSoup(page_content, "html.parser")
+        if get_media(url) is not None:
+            if "video" in get_media(url):
+                return 'VIDEO'
+            elif get_media(url) is not None:
+                return 'IMAGE'
+
+
 # NYT: Gets alt text of first image/video in the article, if it exists
 def nyt_get_alt_text(url):
     req = Request(url, headers={'User-Agent': 'Mozilla/5.0'})
@@ -118,7 +158,7 @@ def nyt_get_alt_text(url):
         return "URL Error"
     else:
         soup = BeautifulSoup(page_content, "html.parser")
-        if get_media_type(url) == "IMAGE":
+        if nyt_get_media_type(url) == "IMAGE":
             for img in soup.find_all('img'):
                 alt = img.find('img', alt=True)
                 if alt is not None:
@@ -152,13 +192,14 @@ def nyt_get_captions_text(url):
 # Writes web page details to outfile
 def printText(url, headline, outfile, website, count):
     media_link = get_media(url)
-    media_type = get_media_type(url)
     if website == "FOX":
         alt_text = (fox_get_alt_text(url))
         caption = fox_get_captions_text(url)
+        media_type = fox_get_media_type(url)
     elif website == "NYT":
         alt_text = nyt_get_alt_text(url)
         caption = nyt_get_captions_text(url)
+        media_type = nyt_get_media_type(url)
     else:
         alt_text = None
         caption = None
@@ -183,7 +224,7 @@ def main():
     count = 1
     for file in os.listdir(directory):
         website = get_website(file)
-        if website != "HPO" and website != "NYT":
+        if website != "HPO":
             url = (get_url(directory + file))
             headline = get_headline(directory + file)
             printText(url, headline, outfile, website, count)
