724809d51ac133501b8055247722052839fa1dc7
mirandadayadkins <mirandaday16@gmail.com>
2020-02-14 15:24:45 -0500

    updated to include unique article IDs

--- Scripts/Main_Entities/caption_main_figures.py
+++ Scripts/Main_Entities/caption_main_figures.py
@@ -3,6 +3,10 @@ import json
 from Scripts import Article_Crawler_Functions
 import csv
 import os
+from Scripts import assigning_article_ids
+
+# Our current directory
+directory = "../../Data/JSON_files/emnlp19_BASIL/data/"
 
 
 def get_main_entities(json_file_path: str):
@@ -17,7 +21,7 @@ def get_main_entities(json_file_path: str):
 # Get captions for articles updated via manual annotation (automatic capture leaves errors)
 def get_updated_captions():
     captions = []
-    with open("../Data/processed_data/metadata.csv", 'rt')as file:
+    with open("../../Data/processed_data/metadata.csv", 'rt')as file:
         data = csv.reader(file)
         for row in data:
             if row[6] != "":
@@ -29,22 +33,23 @@ def get_updated_captions():
 
 def main():
     # Create or open csv file for comparing main entities
-    with open('../Data/processed_data/Main_Entities/caption_main_figures.csv', mode='w') as metadata_table:
+    with open('../../Data/processed_data/Main_Entities/caption_main_figures.csv', mode='w') as metadata_table:
         metadata_writer = csv.writer(metadata_table, delimiter=',', quotechar='"', quoting=csv.QUOTE_MINIMAL)
         # Write header rows
-        metadata_writer.writerow(['Website', 'Headline', 'URL', 'Caption', 'Caption Main Entities',
+        metadata_writer.writerow(['Article ID', 'Website', 'Headline', 'URL', 'Caption', 'Caption Main Entities',
                                   'Article Main Entities', 'Match Rate'])
         count = 1
-        for file in sorted(os.listdir(Article_Crawler_Functions.directory)):
+        for file in sorted(os.listdir(directory)):
             # Assign metadata for each article
             website = Article_Crawler_Functions.get_website(file)
-            headline = Article_Crawler_Functions.get_headline(Article_Crawler_Functions.directory + file)
-            url = (Article_Crawler_Functions.get_url(Article_Crawler_Functions.directory + file))
-            article_entities = get_main_entities(Article_Crawler_Functions.directory + file)
+            article_id = assigning_article_ids.get_article_id(file, website)
+            headline = Article_Crawler_Functions.get_headline(directory + file)
+            url = (Article_Crawler_Functions.get_url(directory + file))
+            article_entities = get_main_entities(directory + file)
             captions = get_updated_captions()
             # Write metadata to file for FOX and NYT articles
             if website != "HPO":
-                metadata_writer.writerow([website, headline, url, captions[count], '', article_entities, ""])
+                metadata_writer.writerow([article_id, headline, url, captions[count], '', article_entities, ""])
                 count += 1
 
 
