bc9f8199e4fd4d5c8ddbfbd3b8591253eda0a44f
mirandadayadkins <mirandaday16@gmail.com>
2020-02-20 15:15:12 -0500

    creates dictionary of aligned articles and metadata

--- Scripts/parallel_articles_list_creator.py
+++ Scripts/parallel_articles_list_creator.py
@@ -4,5 +4,51 @@
 
 import csv
 
-source_csv_file = "/Users/mirandadayadkins/Desktop/Media_Bias/Data/processed_data/" \
-                "Main_Entities/caption_main_figures_edited.csv"
+source_csv_file = "/Users/mirandadayadkins/Desktop/Media_Bias/Data/processed_data/Caption_General_Analysis/" \
+                  "caption_general_analysis_combined.csv"
+
+url_csv_file = "/Users/mirandadayadkins/Desktop/Media_Bias/Data/processed_data/Main_Entities/" \
+               "caption_main_figures_edited.csv"
+
+
+# Returns a dictionary with Article IDs as keys and Headlines as values.
+# Only includes aligned article pairs which both contain media/captions.
+def create_article_id_list():
+    all_ids = []
+    paired_ids = []
+    with open(source_csv_file, 'rt')as file:
+        open_file = csv.reader(file)
+        for row in open_file:
+            all_ids += row[:1]
+    for i in range(len(all_ids) - 1):
+        if all_ids[i][0:2] == all_ids[(i + 1)][0:2]:
+            paired_ids.append(all_ids[i])
+            paired_ids.append(all_ids[i + 1])
+    return paired_ids
+
+
+# Returns a dictionary with Article IDs as keys and URLs as values.
+# Does NOT check for aligned article pairs, as this will be checked in the next function.
+def create_metadata_dict():
+    metadata = {}
+    with open(url_csv_file, 'rt')as file:
+        open_file = csv.reader(file)
+        for row in open_file:
+            # Article ID : Headline, URL
+            metadata[row[0]] = [row[2], row[3]]
+    return metadata
+
+
+def create_combined_dict(id_list, metadata_dict):
+    combined = {}
+    for key in metadata_dict:
+        if key in id_list:
+            combined[key] = metadata_dict[key]
+    return combined
+
+
+def main():
+    print(create_combined_dict(create_article_id_list(), create_metadata_dict()))
+
+
+main()
