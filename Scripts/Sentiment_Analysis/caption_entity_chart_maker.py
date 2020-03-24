c602d1b8f43d568f1d4c4c3c9c2ca47b45522fd0
mirandadayadkins <mirandaday16@gmail.com>
2020-03-24 17:02:27 -0400

    finished basic script

--- Scripts/Sentiment_Analysis/caption_entity_chart_maker.py
+++ Scripts/Sentiment_Analysis/caption_entity_chart_maker.py
@@ -26,3 +26,63 @@ def find_max_main_entities(website):
 # find_max_main_entities('NYT')
 # Returns 4
 
+# Finding the number of captions on each website that contain 0, 1, 2, 3, and 4 main entities (4 is the max; see above)
+# Row 1 is website, Row 5 is caption entities (listed), Row 9 is # of non-main ("Extra") entities in caption
+def get_caption_counts(website):
+    zero = 0
+    one = 0
+    two = 0
+    three = 0
+    four = 0
+    with open(csv_file, 'rt')as file:
+        open_file = csv.reader(file)
+        for row in open_file:
+            if row[1] == website:
+                if row[9] != "-":
+                    if (len(row[5].split(',')) - int(row[9])) == 0:
+                        zero += 1
+                    elif (len(row[5].split(',')) - int(row[9])) == 1:
+                        one += 1
+                    elif (len(row[5].split(',')) - int(row[9])) == 2:
+                        two += 1
+                    elif (len(row[5].split(',')) - int(row[9])) == 3:
+                        three += 1
+                    elif (len(row[5].split(',')) - int(row[9])) == 4:
+                        four += 1
+                else:
+                    if len(row[5].split(',')) == 0:
+                        zero += 1
+                    elif len(row[5].split(',')) == 1:
+                        one += 1
+                    elif len(row[5].split(',')) == 2:
+                        two += 1
+                    elif len(row[5].split(',')) == 3:
+                        three += 1
+                    elif len(row[5].split(',')) == 4:
+                        four += 1
+    print("\n" + website + ":")
+    print("zero: ", zero)
+    print("one: ", one)
+    print("two: ", two)
+    print("three: ", three)
+    print("four: ", four)
+    return [zero, one, two, three, four]
+
+
+get_caption_counts('FOX')
+
+get_caption_counts('NYT')
+
+# FOX:
+# zero:  8
+# one:  54
+# two:  27
+# three:  8
+# four:  3
+#
+# NYT:
+# zero:  0
+# one:  60
+# two:  28
+# three:  9
+# four:  3
